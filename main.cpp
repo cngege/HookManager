dd989be64a41991445bb4eb1fa44ad547bde676e
muzili <2586850402@qq.com>
2024-10-26 04:24:35 +0800

    更新 main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 ﻿// 定义应用程序的入口点。
 //
-#include <iostream>
 #include "HookManager/HookManager.hpp"
+#include <iostream>
 
-int add(int a, int b) {
+HookInstance* h = nullptr;
+
+// hook在Release模式下一定要考虑到内联和优化的问题
+__declspec(noinline) int add(int a, int b) {
 	return a + b;
 }
 
 int hookadd(int a, int b) {
-	return a * b;
+	using fn = int(__fastcall*)(int, int);
+	return ((fn)h->origin)(a, b) * 10;
 }
 
 int main()
 {
+
 	std::cout << "add(5,6):" << add(5, 6) << std::endl;
-	auto h = HookManager::getInstance()->addHook((uintptr_t) & add, &hookadd);
+	h = HookManager::getInstance()->addHook((uintptr_t) & add, &hookadd);
 	h->hook();
 	std::cout << "hooked:" << std::endl;
-	std::cout << "add(5,6):" << add(5, 6) << std::endl;
+	std::cout << "add(5,6):" << add(5, 16) << std::endl;
 
 	std::cout << "removehook:" << std::endl;
 	h->unhook();
