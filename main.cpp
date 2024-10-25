9b457131cd70cc794033e6e629060c61ac4bd91b
muzili <2586850402@qq.com>
2024-10-26 02:43:37 +0800

    main中写入示例

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 ﻿// 定义应用程序的入口点。
 //
 #include <iostream>
+#include "HookManager/HookManager.hpp"
 
-using namespace std;
+int add(int a, int b) {
+	return a + b;
+}
+
+int hookadd(int a, int b) {
+	return a * b;
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "add(5,6):" << add(5, 6) << std::endl;
+	auto h = HookManager::getInstance()->addHook((uintptr_t) & add, &hookadd);
+	h->hook();
+	std::cout << "hooked:" << std::endl;
+	std::cout << "add(5,6):" << add(5, 6) << std::endl;
+
+	std::cout << "removehook:" << std::endl;
+	h->unhook();
+
+	std::cout << "add(5,6):" << add(5, 6) << std::endl;
 	return 0;
 }
